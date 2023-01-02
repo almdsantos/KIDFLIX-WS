5ad0907d138805628027aea9b35ffedc9f0ad040
almdsantos <aliston.m.santos@outlook.com.br>
2023-01-01 23:28:58 -0300

    Problema com a conexão no mongoDB

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 
 const routes = require('./src/routes/filmes.routes');
+mongoose.connect('mongodb://localhost:27017/kidflix', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
